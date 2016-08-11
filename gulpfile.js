d111f24071255ef2e59d132db450aacc5b4e1194
Orie Steele <orie@ohs.io>
2016-08-10 21:32:34 -0500

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function () {
-    return gulp.src('./docs/nml/1.0.0/**/*')
+    return gulp.src('./docs/firedax/1.0.0/**/*')
         .pipe(ghPages());
 });
